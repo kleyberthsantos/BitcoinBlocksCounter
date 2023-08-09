d02fa7bff50282e5e6c8c72e878be024a1b39114
kleyberthsantos <kleysb57@gmail.com>
2023-08-09 14:29:14 -0400

    modifying footer

--- blockcounter/src/components/Footer.jsx
+++ blockcounter/src/components/Footer.jsx
@@ -3,9 +3,8 @@ import React from 'react'
 const Footer = () => {
   return (
     <footer className='footer'>
-      <p>Developed by <a href="https://github.com/kleyberthsantos" target="_blank" rel="noreferrer">Kleyberth Santos</a> y <a href="https://beykerestrada.com" target="_blank" rel="noreferrer">Beyker Estrada</a></p>
+      <p>Developed by <a href="https://github.com/kleyberthsantos" target="_blank" rel="noreferrer">Kleyberth Santos</a> & <a href="https://beykerestrada.com" target="_blank" rel="noreferrer">Beyker Estrada</a></p>
     </footer>
   )
 }
-// colocar rrss para el proximo deploy
 export default Footer
